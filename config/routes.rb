bd53d45a4837fcf88672cebd3191411523c71022
clydotron <bgreen007@cox.net>
2020-03-09 20:21:21 -0700

    added my_friends and some other user stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  
   get 'search_stock', to: 'stocks#search'
 
 end
